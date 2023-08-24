243c0160aadc906cffdc29d19b546c5132f61b28
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2023-08-24 17:28:17 -0400

    Add comments, add logic function

--- scripts.js
+++ scripts.js
@@ -1,13 +1,46 @@
+//Computer makes random choice
+
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
-    let computerSelection = choices[Math.floor(Math.random() * 3)];
+    const computerSelection = choices[Math.floor(Math.random() * 3)];
 
     return computerSelection;
 }
 
+//Player prompted for choice
+
 function getPlayerChoice() {
-    let playerSelection = prompt('Choose Rock, Paper, or Scissors:',);
+    const playerSelection = prompt('Choose Rock, Paper, or Scissors:',);
 
     return playerSelection.charAt(0).toUpperCase()
         + playerSelection.slice(1).toLowerCase();
 }
+
+//Player vs. computer compared to declare winner
+
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors')
+        || (playerSelection === 'Paper' && computerSelection === 'Rock')
+        || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+
+        return 'You win!'
+    }
+
+    else if (playerSelection === computerSelection) {
+        
+        return 'It\'s a tie - clash again!'
+    }
+
+    else {
+        
+        return 'You lose.'
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(`You chose ${playerSelection}.`);
+console.log(`Computer chose ${computerSelection}.`);
+console.log(playRound(playerSelection, computerSelection));
