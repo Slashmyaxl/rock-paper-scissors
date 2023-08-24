d0f39da0e0165346d9f2e6e45a0ad737bc560f23
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2023-08-24 16:26:42 -0400

    Add getPlayerChoice func, case desensitize

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     let computerSelection = choices[Math.floor(Math.random() * 3)];
 
     return computerSelection;
+}
+
+function getPlayerChoice() {
+    let playerSelection = prompt('Choose Rock, Paper, or Scissors:',);
+
+    return playerSelection.charAt(0).toUpperCase()
+        + playerSelection.slice(1).toLowerCase();
 }
